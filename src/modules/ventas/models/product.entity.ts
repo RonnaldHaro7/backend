9a010aeac995dcd056a4c15f7f55f4115db765ca
JohanAlexander15 <johanpavon123@gmail.com>
2023-01-23 14:49:35 -0500

    feature:etity

--- src/modules/ventas/models/product.entity.ts
+++ src/modules/ventas/models/product.entity.ts
@@ -1,5 +1,28 @@
+import { CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
 @Entity('products', {schema:'ventas'} )
 export class ProductEntity{
-    @PrimaryKeyColumn(sv)
+    @PrimaryGeneratedColumn('sv')
     id:string;
+
+    @CreateDateColumn({
+        name:'created_date',
+        type:'timestamptz',
+        default:() => 'CURRENT_TIMESTAMP',
+      })
+      createdAt: Date;
+
+    @UpdateDateColumn({
+      name:'updated_date',
+      type:'timestamptz',
+      default:() => 'CURRENT_TIMESTAMP',
+    })
+    updatedAt: Date;
+    
+    @DeleteDateColumn({
+      name:'deleted_date',
+      type:'timestamptz',
+      default:() => 'CURRENT_TIMESTAMP',
+    })
+    deletedAt: Date;
   }
